f25239902218b19372065597db5a1e402bce5137
wiruj.suw <wiruj.suw@gmail.com>
2025-08-16 21:32:30 +0700

    finish hashmap

---
+++ section_03_collections_errors/ch_03_hashmap/src/main.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert("Team A", 95);
+    scores.insert("Team B", 87);
+    scores.insert("Team C", 92);
+
+    println!("{:?}", scores.get("Team A"));
+
+    match scores.get("Team A") {
+        Some(score) => println!("Team A Score: {}", score),
+        None => println!("Not found Score for Team A")
+    }
+
+    for (team, score) in &scores {
+        println!("{} has {} scores", team, score);
+    }
+    
+    scores.entry("Team D").or_insert(0);
+    
+    println!("--- After at Team D --------------");
+    for (team, score) in &scores {
+        println!("{} has {} scores", team, score);
+    }
+}
